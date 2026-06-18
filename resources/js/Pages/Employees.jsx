789ae6b57f71b7a20c984db9e6cf70f53d1e56c6
MSQ104 <qamarshakeeb@gmail.com>
2026-06-18 17:01:51 +0200

    Initial InertiaJS demo with Laravel and React

---
+++ resources/js/Pages/Employees.jsx
@@ -0,0 +1,10 @@
+export default function Employees({ name, role }) {
+    return (
+        <div>
+            <h1>Employee Page</h1>
+
+            <p>Name: {name}</p>
+            <p>Role: {role}</p>
+        </div>
+    );
+}
